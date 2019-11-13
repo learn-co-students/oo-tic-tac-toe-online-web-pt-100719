716e9550215b013b19f2ea26eaad63f437217fdc
Adrian Bruno <adrian.j.bruno@gmail.com>
2019-11-13 17:36:35 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,15 @@
 class TicTacToe
 
+  # We begin by creating our initialize instance method.
+  # This method passes in the argument of our board as an instance variable
+
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+
+  # Sets our class constant for winning combinations of our Tic Tac Toe game
+  # This is a nested array
+
   WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
@@ -8,82 +18,51 @@ class TicTacToe
     [1,4,7],
     [2,5,8],
     [0,4,8],
-    [2,4,6]
+    [6,4,2]
   ]
 
-  # Declare our initialize method
-  # Sets the value of our board to nil when a new instance
-    # is created.
-  # Instance variable @board = value for 'board' or
-    # an array of nine items with blank elements.
-  def initialize(board = nil)
-    @board = board || Array.new(9, " ")
-  end
+  # Method which displays our board after input is received from the user
 
-  # The method which displays the board
   def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} " # Top row
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} " # Middle row
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} " # Bottom row
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  # Input index
-  # Passes argument of number and subtracts one from
-    # the passed integer => 1 - 1 = 0
+  # Receives input from the user and subtracts the value by one
+
   def input_to_index(user_input)
-    user_input.to_i - 1
+   user_input.to_i - 1
   end
 
   def move(position, char)
     @board[position] = char
   end
 
-  # This method passes in an argument of an index integer
-  # Depending on the passed integer, either a "X" or "O"
-    # will be placed
+  # Determines what positions on the board are taken.
+  # If a position contains a "X" or "O" value, then we get a "true" value
+  # If a position does not contain a "X" or "O" value, then we get a "false" value
 
-  def position_taken?(index_i)
-    ((@board[index_i] == "X") || (@board[index_i] == "O"))
+  def position_taken?(position)
+    if @board[position] == "X" || @board[position] == "O"
+      true
+    else
+      false
+    end
   end
+
+  # Determines whether a move is valid or not
+  # Passes in an index value between 0 and 8
 
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
 
-  # Determines the number of turns by setting the initial count to "0"
-  # Then calls our board array and iterates each space within the array
-  # If space is equal to "X" or space is equal to "O"
-    # add one to our number of turns counter
-  # Display the number of turns
-
-  def turn_count
-    number_of_turns = 0 # Set local variable
-    @board.each do |space|
-      if space == "X" || space == "O"
-        number_of_turns += 1
-      end
-  end
-  return number_of_turns
-
-  def current_player
-    if turn_count % 2 == 0
-      "X"
-    else
-      "O"
-    end
-  end
-
-  # Player is prompted to enter a value: 1-9
-  # Input is collected from player and the value is passed
-    # to the input_to_index method. This value is set to a local variable
-    # called 'index'
-  # Set a local variable 'char' to equal the current_player method
-  # Create if/else statment which calls on the valid_move method
-    # We pass the value for index into the method
-    # If the move is valid, it places "X" or "O" into the Given
-    # index number. The new value of the board is displayed.
+  # Asks the user for a number
+  # Sets the index value to the entered number
+  # If the move is valid, then call the move method and display the board
   # Else, call the turn method
 
   def turn
@@ -99,71 +78,73 @@ class TicTacToe
     end
   end
 
-  # Method which determines the winner
-  # Calls upon the WIN_COMBINATIONS class constant
-  # Checks the board and determines which char is in what
-    # position
-  # If the defined positions contain "X", display win_combo
-  # If the defined positions contain "O", display win_combo
-  # If neither conditions are met, the method returns a false values
+  def turn_count
+    counter = 0
+    @board.each do |i|
+      if i == "X" || i == "O"
+        counter += 1
+      end
+    end
+    counter
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
 
   def won?
-    WIN_COMBINATIONS.detect do |win_combo|
-      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
-        return win_combo
-      elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
-        return win_combo
+
+    board_empty = @board.none? { |i| i == "X" || i = "O"}
+    if board_empty
+      false
+    else
+      WIN_COMBINATIONS.each do |combo|
+      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" || @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+        return combo
       end
-        false
+    end
+    return false
     end
   end
-
-  # Checks to see if all the spots on the board are full
 
   def full?
-    @board.all? do |occupied|
-      occupied != " "
-    end
+    @board.all? { |i| i =="X" || i == "O"}
   end
 
-  # This method checks if the conditions in the
-    # 'won?', 'full?', or 'draw?' methods have been met
+  def draw?
+    !won? && full? ? true : false
+  end
 
   def over?
-    won? || full? || draw?
+    won? || draw? || full? ? true : false
   end
-
-  # Similar to the 'won?' method, this method determines
-    # the game's winner
 
   def winner
-    WIN_COMBINATIONS.detect do |win_combo|
-      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
-        return "X"
-      elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
-        return "O"
-      else
-        nil
-      end
+    WIN_COMBINATIONS.detect do |combo|
+          if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+            return "X"
+          elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+            return "O"
+          else
+            nil
+          end
     end
   end
-
-  # Our play method determines if a game is over or still going
-  # If the game is not over, then call upon the turn method.
-  # If the game is over, a message is put out declaring the winner
-  # If the game results in a draw, put another message
 
   def play
-    while over? == false
-      turn
-    end
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cat's Game!"
-    end
+  while over? == false
+    turn
   end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
 
-  end
+
+
+
+
 
 end
